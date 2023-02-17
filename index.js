68b501b4cf09c9079aae923f6fc2415f9defaee4
thankyoufortherecongnitionchair <77891681+thankyoufortherecongnitionchair@users.noreply.github.com>
2023-02-18 00:30:39 +0530

    changed names

--- index.js
+++ index.js
@@ -10,42 +10,42 @@ for (var looper=0;looper<numDrum;looper++){
         
         switch(buttoninnerhtml){
             case "w":
-                var audio= new Audio("sounds/ohmygoddd.mp3");
+                var audio= new Audio("ohmygoddd.mp3");
                 audio.play();
             break;
             
             case "a":
-                var audio= new Audio("sounds/omg bruh oh hell Naw man wtf man.mp3");
+                var audio= new Audio("omg bruh oh hell Naw man wtf man.mp3");
                 audio.play();
             break;
             
             case "s":
-                var audio= new Audio("sounds/What the hell Oh my God no Way Sound Effect.mp3");
+                var audio= new Audio("What the hell Oh my God no Way Sound Effect.mp3");
                 audio.play();
             break;
             
             case "d":
-                var audio= new Audio("sounds/Boi what the hell boi.mp3");
+                var audio= new Audio("Boi what the hell boi.mp3");
                 audio.play();
             break;
             
             case "j":
-                var audio= new Audio("sounds/Vine boom sound effect.mp3");
+                var audio= new Audio("Vine boom sound effect.mp3");
                 audio.play();
             break;
             
             case "k":
-                var audio= new Audio("sounds/Bing Chilling vine boom.mp3");
+                var audio= new Audio("Bing Chilling vine boom.mp3");
                 audio.play();
             break;
 
             case "l":
-                var audio= new Audio("sounds/full quandale dingle lore.mp3");
+                var audio= new Audio("full quandale dingle lore.mp3");
                 audio.play();
             break;
 
             case "P":
-                var audio= new Audio("sounds/siren meme  sound effect.mp3");
+                var audio= new Audio("siren meme  sound effect.mp3");
                 audio.play();
             break;
             
